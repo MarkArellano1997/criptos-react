66b05429f52f8209214101b91f303e5aa970d67f
Marcos Arellano <140467220+MarkArellano1997@users.noreply.github.com>
2024-02-26 22:49:43 -0500

    first commit

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,38 @@
+import styled from "@emotion/styled"
+import useSelectMonedas from "../hooks/useSelectMonedas"
+import { monedas } from "../data/monedas"
+
+const InputSubmit=styled.input`
+    background-color: #9497FF;
+    border:none;
+    width:100%;
+    padding:10px;
+    color: #FFF;
+    font-weight: 700;
+    text-transform: uppercase;
+    font-size:20px;
+    border-radius:5px;
+    transition: background-color .3s ease;
+
+    &:hover {
+        background-color: #7A7DFE;
+        cursor: pointer;
+    }
+`
+
+export default function Formulario() {
+
+
+    const [SelectMonedas] = useSelectMonedas('Elige tu moneda', monedas)
+
+
+  return (
+   <form>
+
+    <SelectMonedas />
+
+    <InputSubmit type="submit" value='Cotizar'/>
+
+   </form>
+  )
+}
